a18a308cf6fcaf17d6319716daa8cd6481537abe
Yevgeny Levin <devlev1980@gmail.com>
2018-07-27 11:32:35 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'lazy', loadChildren: './lazyloading/lazyloading.module#LazyloadingModule'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/lazyloading/lazyloading.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LazyloadingComponent} from './lazyloading.component';
+import {Routes, RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  {path: '', component: LazyloadingComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  declarations: [
+    LazyloadingComponent
+  ]
+})
+export class LazyloadingModule {
+}
